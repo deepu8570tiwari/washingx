4e2345bad7ea7e1aec8f5d0cd7f851be1ab152e5
Deepu <itsdeeputiwari@gmail.com>
2022-12-07 22:56:24 +0530

    Term and condition pages

--- resources/views/frontend/term-condition.blade.php
+++ resources/views/frontend/term-condition.blade.php
@@ -18,83 +18,37 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-             <h2 style="text-align:center">Our Best Services For You</h2>
-            <div class="row">
-                <div class="col-lg-4 col-xl-4 col-md-6 wow fadeInUp" style="visibility: visible;">
-                    <div class="service-each shadow-2 mb-30 transition-4 text-center">
-                        <a href="" class="black">
-                            <div class="service-icn bg-light-white flex-center">
-                                <img src="img/service/1.png" alt="">
-                            </div>
-                            <div class="service-text">
-                                <h3 class="fs-20 f-700 mb-10">WASH &amp; FOLD</h3>
-                                <p class="mb-0">48 hrs Turnaround time
-                                    Wash &amp; Dry
-                                    Stain Removal
-                                    Cuff &amp; Collar Cleaning
-                                    Fabric Conditioner
-                                    Eco Friendly Combined Packing
-                                    Doorstep Pickup &amp; Delivery</p>
-                                <span class="line-servcie transition-4 bg-blue mt-5"></span>
-                            </div>
-                        </a>
-                    </div>
+            <section id="terms-of-service">
+                <div class="">
+                    <h1 class="primary-heading">Terms of Service</h1>
+                    <p class="paragraph">
+                        <ul>
+                            <li>In consideration of your use of the beCoditive API, you represent that you <span class="bold">are</span> of legal age to form a binding contract and are <span class="bold">not</span> a person barred from receiving services under the laws of the Indian Constitution or other applicable jurisdiction. You also agree to: 
+                                <ul>
+                                    <li> provide true, accurate, current and complete information about yourself as prompted by beCoditive API's registration form and; </li>
+                                    <li>maintain and promptly update the Registration Data to keep it true, accurate, current and complete. If you provide any information that is untrue, inaccurate, not current or incomplete, or beCoditive has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, beCoditive has the right to suspend or terminate your account and refuse any and all current or future use of the beCoditive API (or any portion thereof).</li>
+                                </ul>
+                            </li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>You expressly understand and agree that beCoditive and its subsidiaries, affiliates, officers, employees, agents, partners and licensors shall not be liable to you for any direct, indirect, incidental, special, consequential or exemplary damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if beCoditive has been advised of the possibility of such damages), resulting from the use or the inability to use beCoditive API</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>You acknowledge that beCoditive may establish general practices and limits concerning use of beCoditive API, including without limitation the maximum number of days that email messages, message board postings or other uploaded Content will be retained by beCoditive API. You further acknowledge that beCoditive reserves the right to modify these general practices and limits from time to time. beCoditive reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, beCoditive API (or any part thereof) with or without notice. You agree that beCoditive shall not be liable to you or to any third party for any modification, suspension or discontinuance of beCoditive API.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>You acknowledge that beCoditive may establish general practices and limits concerning use of beCoditive API, including without limitation the maximum number of days that email messages, message board postings or other uploaded Content will be retained by beCoditive API. You further acknowledge that beCoditive reserves the right to modify these general practices and limits from time to time. beCoditive reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, beCoditive API (or any part thereof) with or without notice. You agree that beCoditive shall not be liable to you or to any third party for any modification, suspension or discontinuance of beCoditive API.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            <li>Any kind of abusing, harassment using beCoditive API is. If anyone is found conducting such acts, they will be from the beCoditive API and legal action will be taken against them.</li>
+                            
+                            
+                        </ul>
+                    </p>
                 </div>
-                <div class="col-lg-4 col-xl-4 col-md-6 wow fadeInUp" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s;">
-                    <div class="service-each shadow-2 mb-30 transition-4 text-center">
-                        <a href="" class="black">
-                            <div class="service-icn bg-light-white flex-center">
-                                <img src="img/service/2.png" alt="">
-                            </div>
-                            <div class="service-text">
-                                <h3 class="fs-20 f-700 mb-10">WASH &amp; IRON</h3>
-                                <p class="mb-0">48 hrs Turnaround time
-                                    Wash, Dry &amp; Steam Ironing
-                                    Stain Removal
-                                    Cuff &amp; Collar Cleaning
-                                    Fabric Conditioner
-                                    Eco Friendly Combined Packing
-                                    Doorstep Pickup &amp; Delivery</p>
-                                <span class="line-servcie transition-4 bg-blue mt-5"></span>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-xl-4 col-md-6 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s;">
-                    <div class="service-each shadow-2 mb-30 transition-4 text-center">
-                        <a href="" class="black">
-                            <div class="service-icn bg-light-white flex-center">
-                                <img src="img/service/pinkshoe.png" alt="">
-                            </div>
-                            <div class="service-text">
-                                <h3 class="fs-20 f-700 mb-10">SHOE LAUNDRY</h3>
-                                <p class="mb-0">72 hrs Turnaround time
-                                    Wash, Dry &amp; Steam Ironing
-                                    Stain Removal
-                                    Cuff &amp; Collar Cleaning
-                                    Fabric Conditioner
-                                    Eco Friendly Combined Packing
-                                    Doorstep Pickup &amp; Delivery </p>
-                                <span class="line-servcie transition-4 bg-blue mt-5"></span>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            </section>
         </div> 
-        <div class="col-md-12 ">
-            <div class="row">
-                <div class="col-md-6">
-                    Do you want to get more details
-                </div>
-                <div class="col-md-6">
-                    <button class="btn btn-primary">Contact Us</button>
-                </div>
-            </div>
-            </div>
-        </div>       
     </div>
-    
 </div>
 
 @include('layouts.front-footer')
